5d1603637f920b13cb98d0e1c1503da5f847c4c8
cheng <cheng@LAPTOP-VRB9GM4F>
2020-04-09 20:32:31 +0800

    Test travis

--- config.py
+++ config.py
@@ -1,18 +1,27 @@
 import requests
 
+
 def do_config():
+    post_data={'file_content':'Hello World!'}
+    r = requests.post('http://web',data=post_data)
+    print(r.text)
+
+def check_config(r):
     r = requests.get('http://web')
     print(r.text)
     return r
-def check_config(r):
-    if('Hello World!' in r.text):
-        print('Config success!')
-        return 0
-    else:
-        print('Config failed!')
-        return -1
 
 if __name__ == "__main__":
-    r=do_config()
-    result=check_config(r)
-    exit(result)
+    
+    result1=check_config(r)
+    do_config()
+    result2=check_config(r)
+    
+    target_info='Hello World!'
+
+    if(target_info not in result1.text and target_info in result1.text):
+        print('Config success!')
+        exit 0
+    else:
+        print('Config failed!')
+        exit -1
--- index.py
+++ index.py
@@ -1,7 +1,22 @@
 from flask import Flask
 app = Flask(__name__)
+
+
 @app.route("/")
 def hello():
-    return "Hello World!"
+    with open('1.txt') as f:
+        content=f.read()
+    return content
+
+@app.route("/config", methods=['GET', 'POST'])
+def config():
+    if request.method == 'POST':
+        content=request.form.get('file_content')
+        with open('1.txt','w') as f:
+            f.write(content)
+        return "Config success!"
+    else:
+        return "Need post file_content parameter!"
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int("80"), debug=True)
